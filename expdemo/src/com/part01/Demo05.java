caaac643837dee6d584920b131eecfb0d5811560
xuegeng <xg1375747987@163.com>
2019-04-06 16:41:16 +0800

    expdemo

---
+++ expdemo/src/com/part01/Demo05.java
@@ -0,0 +1,13 @@
+package com.part01;
+
+public class Demo05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String str=null;
+       //将此字符串转换为新的字符数组。
+       str.toCharArray();
+      
+	}
+
+}
